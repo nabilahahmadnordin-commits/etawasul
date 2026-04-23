73f65db11870f23e8f9dc6aa781797db38de5708
Luqman B Ahmad Nordin <luqman.nordin@LPPSA.GOV>
2026-04-23 10:21:35 +0800

    update'

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,5 +15,7 @@ class RoleSeeder extends Seeder
     {
         //
         Role::create(['name' => 'admin']);
+        Role::create(['name' => 'student']);
+        Role::create(['name' => 'kin']);
     }
 }
